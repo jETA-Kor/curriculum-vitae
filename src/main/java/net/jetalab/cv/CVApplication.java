68cb16241552e0e5cdfd097d91ffbc6cd3b00e88
김준철 <jckim@sbcn.co.kr>
2020-01-17 18:29:22 +0900

    Spring Boot 초기화
    Spring Boot를 설치하고 초기화 작업을 진행함.

---
+++ src/main/java/net/jetalab/cv/CVApplication.java
@@ -0,0 +1,13 @@
+package net.jetalab.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CVApplication.class, args);
+	}
+
+}
